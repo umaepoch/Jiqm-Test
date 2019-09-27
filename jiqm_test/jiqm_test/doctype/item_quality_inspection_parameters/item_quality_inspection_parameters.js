00fe7a4dc13145a8a3c483cb0d08b04894f2320f
SureshAlien <sureshnaga96@gmail.com>
2019-09-27 16:44:04 +0530

    item quality inspection parametrs doc added

---
+++ jiqm_test/jiqm_test/doctype/item_quality_inspection_parameters/item_quality_inspection_parameters.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, frappe and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Item Quality Inspection Parameters', {
+	refresh: function(frm) {
+
+	}
+});
